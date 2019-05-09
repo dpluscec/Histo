f4662dfb1ab40aafaff490e9c0c3b536ba24dcfb
Domagoj Pluščec <domagoj.pluscec@fer.hr>
2019-05-09 14:10:50 +0200

    done experiments removed

--- histo/main.py
+++ histo/main.py
@@ -31,12 +31,12 @@ if __name__ == "__main__":
     # experiment
     series_one = [
         # experiments.get_experiment_alexnet_1(data_dict=data_dict, device=device), done
-        experiments.get_experiment_alexnet_2(data_dict=data_dict, device=device),
-        experiments.get_experiment_alexnet_3(data_dict=data_dict, device=device),
-        experiments.get_experiment_alexnet_4(data_dict=data_dict, device=device),
-        experiments.get_experiment_alexnet_5(data_dict=data_dict, device=device),
-        experiments.get_experiment_alexnet_6(data_dict=data_dict, device=device),
-        experiments.get_experiment_resnet_1(data_dict=data_dict, device=device),
+        # experiments.get_experiment_alexnet_2(data_dict=data_dict, device=device), done
+        # experiments.get_experiment_alexnet_3(data_dict=data_dict, device=device),
+        # experiments.get_experiment_alexnet_4(data_dict=data_dict, device=device),
+        # experiments.get_experiment_alexnet_5(data_dict=data_dict, device=device),
+        # experiments.get_experiment_alexnet_6(data_dict=data_dict, device=device),
+        # experiments.get_experiment_resnet_1(data_dict=data_dict, device=device),
         experiments.get_experiment_resnet_2(data_dict=data_dict, device=device),
         experiments.get_experiment_resnet_3(data_dict=data_dict, device=device),
         experiments.get_experiment_resnet_4(data_dict=data_dict, device=device),
