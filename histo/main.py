9e99086a920319fbf7d2d3a12b63422777bbc4a3
Domagoj Pluščec <domagoj.pluscec@fer.hr>
2019-05-10 22:14:03 +0200

    experiments update

--- histo/main.py
+++ histo/main.py
@@ -30,11 +30,11 @@ if __name__ == "__main__":
 
     # experiment
     series_one = [
-        experiments.get_experiment_inception_1(data_dict=data_dict, device=device),
-        experiments.get_experiment_inception_2(data_dict=data_dict, device=device),
-        experiments.get_experiment_inception_3(data_dict=data_dict, device=device),
-        experiments.get_experiment_inception_4(data_dict=data_dict, device=device),
-        experiments.get_experiment_inception_5(data_dict=data_dict, device=device),
+        experiments.get_experiment_inception_6(data_dict=data_dict, device=device),
+        experiments.get_experiment_inception_7(data_dict=data_dict, device=device),
+        experiments.get_experiment_inception_8(data_dict=data_dict, device=device),
+        experiments.get_experiment_inception_9(data_dict=data_dict, device=device),
+        experiments.get_experiment_inception_10(data_dict=data_dict, device=device),
     ]
     for exp in series_one:
         exp.execute()
