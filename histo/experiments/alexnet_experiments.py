217c67fd5bf004ce09a3635c5829f19acd130c1d
Domagoj Pluščec <domagoj.pluscec@fer.hr>
2019-05-30 09:50:57 +0200

    update

--- histo/experiments/alexnet_experiments.py
+++ histo/experiments/alexnet_experiments.py
@@ -223,3 +223,25 @@ def get_experiment_alexnet_10(data_dict, device):
                             optimizer=optimizer, criterion=criterion,
                             device=device, model=model)
     return experiment
+
+
+def get_experiment_alexnet_sgd(data_dict, device):
+    experiment_name = "alexnet_sgd"
+    learn_rate = 1e-3
+    batch_size = 32
+    validation_batch_size = 1024
+    num_epochs = 10
+    weight_decay = 0
+
+    params = ExperimentParameters(lr=learn_rate, batch_size=batch_size,
+                                  validation_batch_size=validation_batch_size,
+                                  num_epochs=num_epochs, weight_decay=weight_decay)
+    model = models.get_alexnet(
+        num_outputs=NUM_CLASSES, pretrained=True, fixed_weights=False)
+    criterion = nn.BCEWithLogitsLoss()
+    optimizer = torch.optim.SGD(
+        params=model.parameters(), lr=params.learn_rate, weight_decay=params.weight_decay)
+    experiment = Experiment(name=experiment_name, params=params, data_dict=data_dict,
+                            optimizer=optimizer, criterion=criterion,
+                            device=device, model=model)
+    return experiment
--- histo/main.py
+++ histo/main.py
@@ -30,18 +30,7 @@ if __name__ == "__main__":
 
     # experiment
     series_one = [
-        experiments.get_experiment_densenet_1(data_dict=data_dict, device=device),
-        experiments.get_experiment_densenet_2(data_dict=data_dict, device=device),
-        experiments.get_experiment_densenet_3(data_dict=data_dict, device=device),
-        experiments.get_experiment_densenet_4(data_dict=data_dict, device=device),
-        experiments.get_experiment_densenet_5(data_dict=data_dict, device=device),
-        experiments.get_experiment_densenet_6(data_dict=data_dict, device=device),
-        experiments.get_experiment_densenet_7(data_dict=data_dict, device=device),
-        experiments.get_experiment_densenet_8(data_dict=data_dict, device=device),
-        experiments.get_experiment_densenet_9(data_dict=data_dict, device=device),
-        experiments.get_experiment_densenet_10(data_dict=data_dict, device=device),
-        experiments.get_experiment_densenet_11(data_dict=data_dict, device=device),
-        experiments.get_experiment_densenet_12(data_dict=data_dict, device=device),
+        experiments.get_experiment_alexnet_sgd(data_dict=data_dict, device=device),
     ]
     for exp in series_one:
         exp.execute()
