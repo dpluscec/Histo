bb14a67ef04daff8f4c6bed3ce367a930b82fb30
Domagoj Pluščec <domagoj.pluscec@fer.hr>
2019-06-09 09:52:13 +0200

    update

--- histo/experiments/densenet_experiments.py
+++ histo/experiments/densenet_experiments.py
@@ -1,9 +1,11 @@
 """Module contains DenseNet experiment definitions."""
 import torch
 import torch.nn as nn
+import torchvision.transforms as transforms
 import histo.models as models
 from histo.experiments.base_experiment import (ExperimentParameters, Experiment,
                                                NUM_CLASSES)
+import histo.dataset as dataset
 
 
 def get_experiment_densenet_1(data_dict, device):
@@ -261,6 +263,50 @@ def get_experiment_densenet_12(data_dict, device):
                                   num_epochs=num_epochs, weight_decay=weight_decay)
     model = models.get_densenet(
         num_outputs=NUM_CLASSES, pretrained=False, fixed_weights=False)
+    criterion = nn.BCEWithLogitsLoss()
+    optimizer = torch.optim.Adam(
+        params=model.parameters(), lr=params.learn_rate, weight_decay=params.weight_decay)
+    experiment = Experiment(name=experiment_name, params=params, data_dict=data_dict,
+                            optimizer=optimizer, criterion=criterion,
+                            device=device, model=model)
+    return experiment
+
+
+def get_experiment_densenet_rotation(device):
+    pcam_train_transform = transforms.Compose([
+        transforms.RandomRotation(degrees=180),
+        transforms.ToTensor(),
+    ])
+    pcam_valid_transform = transforms.Compose([
+        transforms.ToTensor()])
+    pcam_test_transform = transforms.Compose([
+        transforms.ToTensor()
+    ])
+
+    pcam_data_transform = {
+        'train': pcam_train_transform,
+        'valid': pcam_valid_transform,
+        'test': pcam_test_transform
+    }
+    pcam_dataset = dataset.PCamDatasets(data_transforms=pcam_data_transform)
+    train_set = pcam_dataset.train
+    valid_set = pcam_dataset.valid
+    test_set = pcam_dataset.test
+    data_dict = {dataset.TRAIN: train_set, dataset.VALID: valid_set,
+                 dataset.TEST: test_set}
+
+    experiment_name = "densenet_rotation"
+    learn_rate = 1e-4
+    batch_size = 32
+    validation_batch_size = 1024
+    num_epochs = 20
+    weight_decay = 0
+
+    params = ExperimentParameters(lr=learn_rate, batch_size=batch_size,
+                                  validation_batch_size=validation_batch_size,
+                                  num_epochs=num_epochs, weight_decay=weight_decay)
+    model = models.get_densenet(
+        num_outputs=NUM_CLASSES, pretrained=True, fixed_weights=False)
     criterion = nn.BCEWithLogitsLoss()
     optimizer = torch.optim.Adam(
         params=model.parameters(), lr=params.learn_rate, weight_decay=params.weight_decay)
