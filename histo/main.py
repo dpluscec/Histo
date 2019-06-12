6ac4a6fd3102ee6fb66d9c04d7c3bcdbcab82ea5
Domagoj Pluščec <domagoj.pluscec@fer.hr>
2019-06-12 08:53:13 +0200

    hue

--- histo/main.py
+++ histo/main.py
@@ -35,7 +35,7 @@ if __name__ == "__main__":
 
     # experiment
     series_one = [
-        experiments.get_experiment_densenet_hue(device)
+        experiments.get_experiment_densenet_saturation(device)
         # experiments.get_experiment_inception_7(data_dict=data_dict, device=device),
         # experiments.get_experiment_inception_8(data_dict=data_dict, device=device),
         # experiments.get_experiment_inception_9(data_dict=data_dict, device=device),
